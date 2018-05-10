8371c9ffa0da8dd6528cba746bb2d59a49c69d83
saji-ryu <rsajiki@gmail.com>
2018-05-10 17:51:47 +0900

    babel

---
+++ sever.js
@@ -0,0 +1,34 @@
+import * as mosca from "mosca";
+const server = new mosca.Server({
+    port: 1883,
+});
+
+server.on('ready', function(){
+    console.log('Server is ready.');
+});
+
+server.on('clientConnected', function(client){
+    console.log('broker.on.connected.', 'client:', client.id);
+});
+
+server.on('clientDisconnected', function(client){
+    console.log('broker.on.disconnected.', 'client:', client.id);
+});
+
+server.on('subscribed', function(topic, client){
+    console.log('broker.on.subscribed.', 'client:', client.id, 'topic:', topic);
+});
+
+server.on('unsubscribed', function(topic, client){
+    console.log('broker.on.unsubscribed.', 'client:', client.id);
+});
+
+server.on('published', function(packet, client){
+    if (/\/new\//.test(packet.topic)){
+        return;
+    }
+    if (/\/disconnect\//.test(packet.topic)){
+        return;
+    }
+    console.log('broker.on.published.', 'client:', client.id);
+});
