a97d7324a2e2dbd0f03039f57170ad550cce7835
saji-ryu <rsajiki@gmail.com>
2018-06-07 16:23:45 +0900

    port番号

--- src/server.js
+++ src/server.js
@@ -124,8 +124,8 @@ mqttServer.on('published', function (packet, client) {
 
 
 mqttServer.attachHttpServer(httpServer);
-// httpServer.listen(Number(process.env.HTTP_PORT) || 3000);
-httpServer.listen(3000);
+httpServer.listen(Number(process.env.PORT) || 3000);
+//httpServer.listen(3000);
 
 mongoose.connect(mongoURI, () => {
     console.log('connected to mongo');
